fb2d0412822ff1e6418bb0754a6744757e5499c2
Federico <codepalace@fastmail.se>
2022-03-02 17:19:45 +0100

    Initial Commit

---
+++ CustomModifier/CustomModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomModifierApp.swift
+//  CustomModifier
+//
+//  Created by Federico on 02/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct CustomModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
