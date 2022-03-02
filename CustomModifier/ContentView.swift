91020b7b699e8626fe377a08620bba1a9b8ff28c
Federico <codepalace@fastmail.se>
2022-03-02 18:13:59 +0100

    First commit

--- CustomModifier/ContentView.swift
+++ CustomModifier/ContentView.swift
@@ -7,10 +7,44 @@
 
 import SwiftUI
 
+extension View {
+    func addNavigationView(title: String) -> some View {
+        NavigationView {
+            self
+                .navigationTitle(title)
+        }
+    }
+    
+    func centerH() -> some View {
+        HStack {
+            Spacer()
+            self
+            Spacer()
+        }
+    }
+    
+    func specialFont() -> some View {
+        self
+            .font(.system(size: 20, weight: .bold, design: .monospaced))
+            .foregroundColor(.indigo)
+    }
+    
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Form {
+            Text("Hello, world!")
+                .specialFont()
+                .centerH()
+            
+            Button("Press me") {
+                print("Button pressed.")
+            }
+            .centerH()
+        }
+        .addNavigationView(title: "Settings")
+
     }
 }
 
